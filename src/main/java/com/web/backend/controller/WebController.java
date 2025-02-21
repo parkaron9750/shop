416c9a99e5fccb394c0a1a63be0c7f252dc570df
parkaron <me9750@naver.com>
2025-02-21 15:42:23 +0900

    Shop thymeleaf footer header 추가

--- src/main/java/com/web/backend/controller/WebController.java
+++ src/main/java/com/web/backend/controller/WebController.java
@@ -1,13 +1,20 @@
 package com.web.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebController {
 
     @GetMapping("/hello")
     public String hello(){
         return "Hello World";
     }
+
+
+    @GetMapping("/test")
+    public String test(){
+        return "layouts/layout";
+    }
+
 }
